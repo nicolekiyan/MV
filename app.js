e804aeda31f394ba03f5bd5399cbbbc54876c655
Nicole <159541580+nicolekiyan@users.noreply.github.com>
2024-10-03 11:19:12 -0300

    update

---
+++ app.js
@@ -0,0 +1,83 @@
+const menu = [
+   {
+     id: 1,
+    title: "Blond by Frank Ocean",
+    category: "rbsoul",
+    price: 44.88,
+    img:
+    desc: "Frank Ocean's album, Blonde, symbolizes duality and ambiguity, reflecting on his personal experiences, identity, and the fluidity of memory."
+},
+{
+    id: 2,
+    title: "SOS by SZA",
+    category: "rbsoul",
+    price: 53.90,
+    img:
+    desc: "SOS by SZA, is a sophomore album, showcases her evolution as an artist and explores themes of love, heartbreak, and self-discovery. With a blend of R&B, hip-hop, and pop, the album features introspective lyrics and emotional vulnerability."
+},
+{
+    id: 3,
+    title: "Sade Love Deluxe",
+    category: "rbsoul",
+    price: 73.90,
+    img:
+    desc: "Love Deluxe, released in 1992, is Sade's fourth studio album and a hallmark of sophisticated R&B. hematically, it explores love, longing, and emotional intimacy, with tracks that range from the hauntingly beautiful 'No Ordinary Love' to the seductive 'Cherish the Day'."
+},
+{
+    id: 4,
+    title: "Red moon in Venus",
+    category: "rbsoul",
+    price: 55.80,
+    img:
+    desc: "'Red Moon in Venus', released by Kali Uchis in 2023, is a captivating exploration of love, desire, and self-empowerment. The album blends elements of R&B, pop, and reggaeton, showcasing Uchis’ eclectic style and rich vocal delivery.",
+},
+{
+    id: 5,
+    title: "I can't stop loving you by Ray Charles",
+    category: "rbsoul",
+    price: 99.90,
+    img:
+    desc: "'I Can't Stop Loving You,' famously performed by Ray Charles, is a heartfelt ballad that expresses deep, unwavering love despite the pain of separation. Released in 1962 as part of his album 'Modern Sounds in Country and Western Music,' the song showcases Charles' signature blend of soul and country influences."
+},
+{
+    id: 6,
+    title: "I can't stop loving you by Ray Charles",
+    category: "jazz",
+    price: 99.90,
+    img:
+    desc: "'I Can't Stop Loving You,' famously performed by Ray Charles, is a heartfelt ballad that expresses deep, unwavering love despite the pain of separation. Released in 1962 as part of his album 'Modern Sounds in Country and Western Music,' the song showcases Charles' signature blend of soul and country influences."
+},
+{
+    id: 7,
+    title: "Ninas'Back by Nina Simone",
+    category: "rbsoul",
+    price: 88.50,
+    img:
+    desc: "'Nina's Back,' released in 1985, marks a significant return for Nina Simone, showcasing her unique blend of jazz, blues, and soul. The album features her powerful, emotive vocals and sharp lyrical insights, reflecting her experiences and perspectives on love, identity, and social issues."
+},
+{
+    id: 8,
+    title: "Ultimate Sinatra by Frank Sinatra",
+    category: "jazz",
+    price: 160.90,
+    img:
+    desc: "'Ultimate Sinatra,' released in 2015, is a comprehensive compilation that celebrates Frank Sinatra's illustrious career. The album features a selection of his most iconic tracks, spanning various phases of his musical journey. From early classics like 'Fly Me to the Moon' to later hits such as 'My Way,' it showcases Sinatra's evolution as an artist."
+},
+{
+    id: 9,
+    title: "The Music Never Stops by Betty Carter",
+    category: "jazz",
+    price: 90.50,
+    img:
+    desc: "The Music Never Stops by Betty Carter is a captivating album that showcases her exceptional vocal talent and improvisational skills. Released in 1992, it features a mix of jazz standards and original compositions, highlighting Carter's ability to infuse each song with her unique style and emotional depth."
+},
+{
+    id: 10,
+    title: "Blossom Dearie",
+    category: "jazz",
+    price: 150.00,
+    img:
+    desc: "Blossom Dearie is a delightful album by the jazz vocalist and pianist, known for her distinctive voice and charming style. Released in 1956, the album features a collection of standards and original songs, showcasing Dearie's unique ability to blend wit and emotion in her performances."
+}
+
+]
